03deff33d3fe9a8e15640dc2ad7130577a57b8f6
NetNegativa <disik123@gmail.com>
2022-04-29 14:36:44 +0700

    init comit

---
+++ main.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+d = 0
+
+if a >= 0:
+    d += a
+
+if b >= 0:
+    d += b
+
+if c >= 0:
+    d += c
+
+print(d)
---
+++ mymodule.py
@@ -0,0 +1,8 @@
+name= "Bob"
+def hello():
+    print('Hello')
+def add_three_numbers(a, b, c):
+    if a != 0 and b != 0 and c != 0:
+        return a + b + c
+    else:
+        return "some is zero"
