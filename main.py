4345b333d1c7e607640e8f55e46ba75d86458f71
NetNegativa <disik123@gmail.com>
2022-04-29 14:59:39 +0700

    init comit

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 a = int(input())
-b = int(input())
-c = int(input())
 
-d = 0
-
-if a >= 0:
-    d += a
-
-if b >= 0:
-    d += b
-
-if c >= 0:
-    d += c
-
-print(d)
+if -1 < a < 17:
+    print('Принадлежит')
+else:
+    print('Не принадлежит')
