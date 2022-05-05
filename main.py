52e1bb28d6238369f36729985b71ede8a0219ba5
NetNegativa <disik123@gmail.com>
2022-05-05 13:01:10 +0700

    init comit

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ b1 = int(input())
 a2 = int(input())
 b2 = int(input())
 
-if (a1 + b1 + a2 + b2) % 2 == 0:
-    print('NO')
+if (a1 - b1 == a2 - b2) or (a1 + b1 == a2 + b2):
+    print('YES')
+elif a1 == a2 or b1 == b2:
+    print('YES')
 else:
-    if (abs(a1 - a2) == 1 and abs(b1 - b2) == 2) or (abs(a1 - a2) == 2 and abs(b1 - b2) == 1):
-        print('YES')
-    else:
-        print('NO')
+    print('NO')
