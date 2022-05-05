6721b8a2004c9c909ed60b20188eacd35fa2cc0d
NetNegativa <disik123@gmail.com>
2022-05-05 15:53:38 +0700

    init comit

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-a = float(input())
+from math import sqrt
 
-print((int(a * 10)) % 10)
+a = float(input())
+b = float(input())
+c = float(input())
+
+d = b ** 2 - 4 * a * c
+if d > 0:
+    x1 = ((-b + sqrt(d)) / (2 * a))
+    x2 = ((-b - sqrt(d)) / (2 * a))
+    print(min(x1, x2))
+    print(max(x1, x2))
+elif d == 0:
+    x = (-b / (2 * a))
+    print(x)
+else:
+    print('Нет корней')
