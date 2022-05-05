89d4036c4a7fa0b664dfe0d8b102ffd5086a31d0
NetNegativa <disik123@gmail.com>
2022-05-05 12:44:48 +0700

    init comit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ b1 = int(input())
 a2 = int(input())
 b2 = int(input())
 
-if (a1 + b1 + a2 + b2) % 2 == 0:
+if (a1 - b1 == a2 - b2) or (a1 + b1 == a2 + b2):
     print('YES')
 else:
     print('NO')
