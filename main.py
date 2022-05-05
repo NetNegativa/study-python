4ada9e9cd435b341accc8be0fdb68102817e49c9
NetNegativa <disik123@gmail.com>
2022-05-05 14:39:24 +0700

    init comit

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-a1 = int(input())
-b1 = int(input())
-a2 = int(input())
-b2 = int(input())
+a = float(input())
 
-if (a1 - b1 == a2 - b2) or (a1 + b1 == a2 + b2):
-    print('YES')
-elif a1 == a2 or b1 == b2:
-    print('YES')
-else:
-    print('NO')
+print((int(a * 10)) % 10)
