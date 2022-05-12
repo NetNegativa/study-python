79a40a5be07dc4ba28e30c56ae5b577b6783fd29
NetNegativa <disik123@gmail.com>
2022-05-12 14:01:29 +0700

    init comit

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-n = int(input())
-b = 0
+a = int(input())
+f1 = 0
+f2 = 1
 
-for i in range(n + 1):
-    if i % 2 == 0:
-        b -= i
-    else:
-        b += i
-print(b)
+for i in range(a):
+    f1, f2 = f2, f1 + f2
+    print(f1, end=' ')
