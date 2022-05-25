567342e8c029cc693e0ddc9b20ffe583cb843576
NetNegativa <disik123@gmail.com>
2022-05-25 15:02:29 +0700

    init comit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 num = int(input())
 
-for i in range(num // 2 + 1):
-    for j in range(i):
-        print('*', end='')
-    print()
-for k in range(num // 2 + 1, 0, -1):
-    for m in range(k):
-        print('*', end='')
-    print()
+while num > 9:
+    sum = 0
+    while num != 0:
+        sum += num % 10
+        num = num // 10
+    num = sum
+
+print(num)
